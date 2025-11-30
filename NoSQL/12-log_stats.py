6f0b77e5154bddba7c957f098a6ddc13ed1cc405
shukufa <shukufa@kali.kali>
2025-11-30 09:21:07 -0500

    NoSQL

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
 """
-Script that provides stats about Nginx logs stored in MongoDB.
+Python script that provides stats about Nginx logs stored in MongoDB
 """
 
 from pymongo import MongoClient
 
 def main():
     # Connect to MongoDB
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient('mongodb://localhost:27017')
     db = client.logs
-    collection = db.nginx
+    nginx = db.nginx
 
-    # Total number of documents
-    total_logs = collection.count_documents({})
+    # Total number of logs
+    total_logs = nginx.count_documents({})
     print(f"{total_logs} logs")
 
-    # List of HTTP methods to count
+    # Methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-
     for method in methods:
-        count = collection.count_documents({"method": method})
+        count = nginx.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # Count documents with GET /status
-    get_status_count = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{get_status_count} status check")
+    # Number of GET requests to /status
+    status_count = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
 
 if __name__ == "__main__":
     main()
